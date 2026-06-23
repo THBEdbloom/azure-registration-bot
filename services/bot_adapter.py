c9b850a683fb9447e4db2dc943529d7b89cfe798
Laurin Krüger <krueger@th-brandenburg.de>
2026-06-23 21:28:41 +0200

    Configure Bot Framework tenant

--- services/bot_adapter.py
+++ services/bot_adapter.py
@@ -7,36 +7,26 @@ from botbuilder.core import (
 
 from services.keyvault_service import get_secret
 
+
 USE_KEY_VAULT = (
     os.getenv("USE_KEY_VAULT", "false").lower()
     == "true"
 )
 
 if USE_KEY_VAULT:
-
-    APP_ID = get_secret(
-        "bot-app-id"
-    )
-
-    APP_PASSWORD = get_secret(
-        "bot-app-password"
-    )
-
+    APP_ID = get_secret("bot-app-id")
+    APP_PASSWORD = get_secret("bot-app-password")
+    APP_TENANT_ID = get_secret("bot-tenant-id")
 else:
+    APP_ID = os.getenv("BOT_APP_ID", "")
+    APP_PASSWORD = os.getenv("BOT_APP_PASSWORD", "")
+    APP_TENANT_ID = os.getenv("BOT_TENANT_ID", "")
 
-    APP_ID = os.getenv(
-        "BOT_APP_ID",
-        ""
-    )
-
-    APP_PASSWORD = os.getenv(
-        "BOT_APP_PASSWORD",
-        ""
-    )
 
 SETTINGS = BotFrameworkAdapterSettings(
     APP_ID,
-    APP_PASSWORD
+    APP_PASSWORD,
+    channel_auth_tenant=APP_TENANT_ID
 )
 
 ADAPTER = BotFrameworkAdapter(
